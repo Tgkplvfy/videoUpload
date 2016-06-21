d76d5f0d661f8ce9853f926ac1c0c3281f39619c
尨喵 <id0o0bi.org@gmail.com>
2016-06-21 14:55:11 +0800

    testing git on server

--- app/controllers/Index.php
+++ app/controllers/Index.php
@@ -7,7 +7,7 @@ class IndexController extends Ap_Base_Control {
 
     public function IndexAction () 
     {
-        echo 'hello restful api';
+        echo 'hello restful api, we are using github now';
         // print_r($this->actions);
     }
     
